48c05550d08d2431e9ef6fbae885dd00684e1d2a
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2024-04-01 11:42:18 +0200

    iOs: Browse > Search: Hide cancel button that appears when typing via SearchBarHandler

--- src/BluOsNadRemote.App/Platforms/iOS/AppDelegate.cs
+++ src/BluOsNadRemote.App/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,21 @@
 ﻿using Foundation;
+using Microsoft.Maui.Handlers;
 
 namespace BluOsNadRemote.App;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        var app = MauiProgram.CreateMauiApp();
+
+        //Hack: remove cancel button on iOs that does nothing https://github.com/dotnet/maui/issues/13720
+        SearchBarHandler.Mapper.AppendToMapping("CancelButtonColor", (searchBarHandler, searchBar) =>
+        {
+            searchBarHandler.PlatformView.SetShowsCancelButton(false, false);
+        });
+
+        return app;
+    }
 }

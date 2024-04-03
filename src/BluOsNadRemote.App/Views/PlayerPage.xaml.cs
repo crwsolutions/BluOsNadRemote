eed22a6928dcfd312d6c3f112cc19ae11679c5bb
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2024-04-03 16:34:30 +0200

    Styled more button (as GlyphButton).

--- src/BluOsNadRemote.App/Views/PlayerPage.xaml.cs
+++ src/BluOsNadRemote.App/Views/PlayerPage.xaml.cs
@@ -14,7 +14,7 @@ public partial class PlayerPage : BaseContentPage
         min = Math.Min(min, 540);
 #endif
         base.OnSizeAllocated(width, height);
-        const double margin = 8 + 66 + 8;
+        const double margin = 8 + 8 + 58 + 8;
         AlbumImage.WidthRequest = min - margin;
         AlbumImage.MaximumWidthRequest = min - margin;
         AlbumImage.HeightRequest = min - margin;

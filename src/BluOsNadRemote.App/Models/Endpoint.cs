b858164dd8c4d4bc900879cb0d5ecdfeec01ecec
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2025-05-05 13:52:04 +0200

    Update Equals method to use nullable parameter type
    Refactor the `Equals` method in the `EndPoint` class to accept a nullable `object?` parameter instead of `object`.

--- src/BluOsNadRemote.App/Models/Endpoint.cs
+++ src/BluOsNadRemote.App/Models/Endpoint.cs
@@ -18,7 +18,7 @@ public sealed class EndPoint
 
     public string LastKnowName { get; set; }
 
-    public override bool Equals(object obj)
+    public override bool Equals(object? obj)
     {
         if (obj is EndPoint endPoint)
         {

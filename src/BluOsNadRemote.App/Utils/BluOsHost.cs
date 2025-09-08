56348742d66a1c1f0d3b02070c68c5fd6b8fa3f7
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2025-09-08 13:27:23 +0200

    Refactor endpoint handling and URI generation. Introduced BluOsHost class in BluOsHost.cs to centralize URI string creation for IPv4 and IPv6 addresses

---
+++ src/BluOsNadRemote.App/Utils/BluOsHost.cs
@@ -0,0 +1,17 @@
+﻿using Blu4Net;
+
+namespace BluOsNadRemote.App.Utils;
+internal static class BluOsHost
+{
+    internal static string ToUriString(string host)
+    {
+        // Check if the host is an IPv6 address
+        if (System.Net.IPAddress.TryParse(host, out var ipAddress) && ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+        {
+            // For IPv6 addresses, wrap in square brackets
+            return $"http://[{host}]:{BluEnvironment.DefaultEndpointPort}/";
+        }
+        // For IPv4 addresses or hostnames, use normal format
+        return $"http://{host}:{BluEnvironment.DefaultEndpointPort}/";
+    }
+}
